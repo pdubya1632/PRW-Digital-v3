c8645187c4ed0a1410e64ecc5dcef252b93f585f
Patrick Richardson Wright <wrightmight@gmail.com>
2022-08-28 21:57:57 -0700

    add error boundary for admin slug

--- app/routes/projects/admin/$slug.tsx
+++ app/routes/projects/admin/$slug.tsx
@@ -180,3 +180,15 @@ export function CatchBoundary() {
   }
   throw new Error(`Unsupported thrown response status code: ${caught.status}`);
 }
+
+export function ErrorBoundary({ error }: { error: unknown }) {
+  if (error instanceof Error) {
+    return (
+      <div className="text-red-500">
+        Oops! Something went wrong.
+        <pre>{error.message}</pre>
+      </div>
+    );
+  }
+  return <div className="text-red-500">Oops! Something went wrong.</div>;
+}

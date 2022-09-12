237b36f18f3d3d87344f0f0e2eca67f45edf45ab
Patrick Richardson Wright <wrightmight@gmail.com>
2022-09-12 09:50:17 -0700

    add link to thumb

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -24,7 +24,7 @@ export default function Index() {
         <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 p-7">
           {projects.map((project) => (
             <div className="card card-compact card-bordered flex-wrap" key={project.slug}>
-              <figure><img src={project.cover} alt={project.title} /></figure>
+              <figure><a href={project.url}><img src={project.cover} alt={project.title} /></a></figure>
               <div className="card-body p-7">
                 <h2 className="card-title">
                   <a href={project.url}>

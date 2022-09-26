029c30f8cc18cde97933d4597922e3edd19c9fba
Patrick Richardson Wright <wrightmight@gmail.com>
2022-09-26 16:01:30 -0700

    update email address on contact page

--- app/config/constants.ts
+++ app/config/constants.ts
@@ -1,15 +1,15 @@
 export const SITE_AUTHOR = `Patrick Richardson Wright`;
 export const SITE_DESCRIPTION = `The online portfolio of ${SITE_AUTHOR}, a Creative Developer`;
-export const SITE_EMAIL_ADDRESS = `wrightmight@gmail.com`;
+export const SITE_EMAIL_ADDRESS = `hello@prw.digital`;
 export const SITE_EMAIL_LINK = `mailto:${SITE_EMAIL_ADDRESS}`;
 export const SITE_TITLE = `${SITE_AUTHOR} - A Creative Developer`;
-export const SITE_UPDATED = new Date("2022-07-20");
+export const SITE_UPDATED = new Date('2022-07-20');
 export const SITE_URL = `https://prw.digital`;
 export const SITE_YEAR = new Date().getFullYear();
 
 export const INTRO_BLURB = `I'm a Creative Developer with a love for great design, quality code,
           and intuitive user experiences.`;
-          export const PROFILE_PIC = `https://fvhsvmaekuyarkujwasn.supabase.co/storage/v1/object/public/portfolio/selfies/about-profile-1.jpg`;
+export const PROFILE_PIC = `https://fvhsvmaekuyarkujwasn.supabase.co/storage/v1/object/public/portfolio/selfies/about-profile-1.jpg`;
 
 export const SOCIAL_GITHUB = `https://github.com/pdubya1632/`;
 export const SOCIAL_LINKEDIN = `https://www.linkedin.com/in/patrick-wright-451535166/`;
